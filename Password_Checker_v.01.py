3eddbb1f275b4fde700f1e1fa1f6a18a50cb5d44
kayracizmeci <kayracizmeci@gmail.com>
2025-12-01 18:04:34 +0300

    Add password length validation and confirmation
    Implement password validation and re-entry checks.

---
+++ Password_Checker_v.01.py
@@ -0,0 +1,41 @@
+# Start
+
+import time
+
+print('Password must be atleast 8 letters.')
+
+# Password Entering And Loop
+
+while True:
+    password = input('Password: ')
+
+# Password 8 Characters Long Control
+
+
+    if len(password) < 8:
+        print('Password must be atleast 8 characters!')
+        continue
+    elif len(password) > 8 or len(password) == 8:
+        break
+        condition_1 = False
+
+# Re-Entering Password
+
+
+while True:
+    print('Please re-enter your password.')
+    password_check = input('Password: ')
+
+    if password_check != password:
+       print('Incorrect password please try again.')
+
+       continue
+
+
+    elif password_check == password:
+        print('Correct password.')
+        condition_2 = False
+        break
+
+
+
